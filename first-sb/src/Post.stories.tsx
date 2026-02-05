ee66587680ce5c6debd578d2fcff96d5c9ea7589
alexhddev <21276888+alexhddev@users.noreply.github.com>
2026-02-05 13:26:59 +0000

    First interraction test

--- first-sb/src/Post.stories.tsx
+++ first-sb/src/Post.stories.tsx
@@ -1,19 +1,38 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import Post from './Post'
+import { expect } from 'storybook/test'
 
 const meta = {
     title: 'Cool/Post',
     component: Post,
+    // default args
+    args: {
+        content: 'The content of a cool story',
+        title: 'Cool Story',
+        onTagAdded: () => { },
+        onTagRemoved: () => { }
+    }
 } satisfies Meta<typeof Post>
 
 export default meta
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {
+export const Base: Story = {}
+
+export const WithInitialTagsTest: Story = {
     args: {
-        content: 'The content of a cool story',
-        title: 'Cool Story',
-        onTagAdded: ()=>{},
-        onTagRemoved: ()=>{}
+        initialTags: ['Cool', 'Pretty']
+    },
+    play: async ({ canvas }) => {
+        const prettyTag = await canvas.findByText('Pretty', {
+            selector: 'div'
+        })
+        const coolTag = await canvas.findByText('Cool', {
+            selector: 'div'
+        })
+
+        await expect(prettyTag).toBeInTheDocument()
+        await expect(coolTag).toBeInTheDocument()
+
     }
 }
