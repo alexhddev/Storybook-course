69a5569fd2db1c3261a260bc0cd4688ea584097e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2026-02-10 07:53:11 +0000

    Mocking function calls

--- first-sb/src/mocks/simple/Names.stories.tsx
+++ first-sb/src/mocks/simple/Names.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Name } from './Name';
+import { mocked } from 'storybook/test';
+import { getRandomName } from './Utils'
 
 const meta = {
     title: 'Mocks/Name',
@@ -14,6 +16,13 @@ type Story = StoryObj<typeof meta>;
  * Random values
  */
 export const Default: Story = {};
+
+
+export const WithMockData: Story = {
+    beforeEach: ()=>{
+        mocked(getRandomName).mockReturnValue('Fixed Name')
+    }
+};
 
 
 
