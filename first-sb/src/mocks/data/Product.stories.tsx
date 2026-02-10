aa8132ad9f04b56eb2ed2fe997b8d79f9a13581a
alexhddev <21276888+alexhddev@users.noreply.github.com>
2026-02-10 09:44:08 +0000

    MSW stories

--- first-sb/src/mocks/data/Product.stories.tsx
+++ first-sb/src/mocks/data/Product.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Products } from './Products'
+import { type ProductsResponse } from './DataService'
+import { http, HttpResponse, delay } from 'msw'
 
 const meta = {
     title: 'Example/Products',
@@ -10,3 +12,57 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+const mockData: ProductsResponse = {
+    success: true,
+    data: [
+        {
+            name: 'Mock product 1',
+            price: 'Mock price 1'
+        },
+        {
+            name: 'Mock product 2',
+            price: 'Mock price 2'
+        }
+    ]
+}
+
+export const WithMockData: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+               http.get('http://localhost:4000/products', ()=>{
+                return HttpResponse.json(mockData)
+               }) 
+            ]
+        }
+    }
+}
+
+export const WithNetworkError: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+               http.get('http://localhost:4000/products', ()=>{
+                return HttpResponse.json(
+                    {error: 'Mock error'},
+                    {status: 401}
+                )
+               }) 
+            ]
+        }
+    }
+}
+
+export const WithLoadingData: Story = {
+    parameters: {
+        msw: {
+            handlers: [
+               http.get('http://localhost:4000/products',async ()=>{
+                await delay('infinite')
+                return HttpResponse.json(mockData)
+               }) 
+            ]
+        }
+    }
+}
